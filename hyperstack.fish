ebe3566cefcccd288faa000cfe9bda298542cc5d
Paul Buetow <paul@buetow.org>
2026-03-21 09:55:02 +0200

    add fish

---
+++ hyperstack.fish
@@ -0,0 +1,2 @@
+abbr pi-hyperstack-nemotron pi --model hyperstack1/cyankiwi/NVIDIA-Nemotron-3-Super-120B-A12B-AWQ-4bit
+abbr pi-hyperstack-coder pi --model hyperstack2/bullpoint/Qwen3-Coder-Next-AWQ-4bit
